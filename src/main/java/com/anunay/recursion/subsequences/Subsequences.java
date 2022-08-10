adfce1afa879aca664741fbc837d18b66afb9ea9
sinha86 <sinha86@avaya.com>
2022-08-10 18:00:37 +0530

    Added code for returning ArrayList of subsequences of a given string

--- src/main/java/com/anunay/recursion/subsequences/Subsequences.java
+++ src/main/java/com/anunay/recursion/subsequences/Subsequences.java
@@ -1,5 +1,8 @@
 package com.anunay.recursion.subsequences;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Subsequences {
 
     private static final String EMPTY = "EMPTY_STRING";
@@ -8,6 +11,10 @@ public class Subsequences {
         String str = "abc";
         System.out.format("Subsequences of %s:\n", str);
         printSubsequences(str, "");
+
+        // ArrayList of subsequences
+        ArrayList<String> subsequenceList = getSubsequenceList(str, "");
+        System.out.format("ArrayList of subsequences of %s: %s", str, subsequenceList);
     }
 
     /**
@@ -30,5 +37,36 @@ public class Subsequences {
         printSubsequences(given.substring(1), processed + currentCharacter);
         // Ignore current character
         printSubsequences(given.substring(1), processed);
+    }
+
+    /**
+     * Return an ArrayList of subsequences
+     * Without passing the list as argument
+     */
+    static ArrayList<String> getSubsequenceList(String given, String processed) {
+
+        // Create a new local ArrayList
+        ArrayList<String> subsequenceList = new ArrayList<>();
+
+        // Base condition: If given string is empty, print the processed string
+        if (given.isEmpty()) {
+            if (processed.isEmpty())
+                subsequenceList.add(EMPTY);
+            else
+                subsequenceList.add(processed);
+            return subsequenceList;
+        }
+
+        // Get the current character
+        Character currentCharacter = given.charAt(0);
+        // Take current character and store the left function call's return value
+        ArrayList<String> left = getSubsequenceList(given.substring(1), processed + currentCharacter);
+        // Ignore current character and store the right function call's return value
+        ArrayList<String> right = getSubsequenceList(given.substring(1), processed);
+
+        // Finally return a mix of left and right function calls' return value
+        subsequenceList.addAll(left);
+        subsequenceList.addAll(right);
+        return subsequenceList;
     }
 }
