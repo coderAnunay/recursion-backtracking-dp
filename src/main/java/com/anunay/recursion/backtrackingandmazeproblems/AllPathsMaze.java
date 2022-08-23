e37034b3944569893931d926455c9ee035c567f8
sinha86 <sinha86@avaya.com>
2022-08-23 12:47:55 +0530

    Added Backtracking Maze Example

--- src/main/java/com/anunay/recursion/backtrackingandmazeproblems/AllPathsMaze.java
+++ src/main/java/com/anunay/recursion/backtrackingandmazeproblems/AllPathsMaze.java
@@ -1,5 +1,7 @@
 package com.anunay.recursion.backtrackingandmazeproblems;
 
+import java.util.Arrays;
+
 /**
  * Given a 3x3 Maze, with movement allowed in all directions (Up, Down, Left and Right)
  *
@@ -18,6 +20,9 @@ public class AllPathsMaze {
 
     public static void main(String[] args) {
         printAllPaths(0,0,"", MAZE_3_BY_3);
+        System.out.println("-------All paths and Matrix------");
+        int[][] path = new int[MAZE_3_BY_3.length][MAZE_3_BY_3[0].length];
+        printAllPathsAndMatrix(0, 0, "", MAZE_3_BY_3, path, 1);
     }
 
     /**
@@ -61,5 +66,54 @@ public class AllPathsMaze {
         // This line is where the function call will be over
         // before function returns, also remove the changes made by the function (backtracking)
         maze[r][c] = true;
+    }
+
+    static void printAllPathsAndMatrix(int r, int c, String processed, boolean[][] maze, int[][] path, int step) {
+        // Base condition: When r=2 & c=2,
+        // Then print processed as we have found an answer
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            System.out.println(processed);
+            path[r][c] = step;
+            for (int[] arr : path) {
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println();
+            return;
+        }
+
+        if (!maze[r][c]) {
+            return;
+        }
+
+        // I am considering this cell in my path
+        maze[r][c] = false;
+
+        // The current cell in my path will be the step number
+        path[r][c] = step;
+
+        if (r < maze.length - 1) {
+            // Go down
+            printAllPathsAndMatrix(r+1, c, processed + "D", maze, path, step+1);
+        }
+
+        if (r > 0) {
+            // Go up
+            printAllPathsAndMatrix(r-1, c, processed + "U", maze, path, step+1);
+        }
+
+        if (c < maze[0].length - 1) {
+            // Go left
+            printAllPathsAndMatrix(r, c+1, processed + "R", maze, path, step+1);
+        }
+
+        if (c > 0) {
+            // Go right
+            printAllPathsAndMatrix(r, c - 1, processed + "L", maze, path, step+1);
+        }
+
+        // This line is where the function call will be over
+        // before function returns, also remove the changes made by the function (backtracking)
+        maze[r][c] = true;
+        path[r][c] = 0;
     }
 }
